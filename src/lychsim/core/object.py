6ce9cd12f8634c5caf551067471ad8b4a5091982
Wufei Ma <wufeim@gmail.com>
2025-06-29 13:19:53 -0400

    add lychsim.core.object

---
+++ src/lychsim/core/object.py
@@ -0,0 +1,20 @@
+__all__ = ['Object']
+
+
+class Object:
+    """LychSim Object.
+    """
+    def __init__(
+        self, name=None, uid=None, obb=None
+    ):
+        self.name = name
+        self.uid = uid
+        self.obb = obb
+
+    def to_dict(self):
+        """Returns a dictionary representation of the Object.
+        """
+        return dict(
+            name=self.name,
+            uid=self.uid,
+            obb=self.obb.to_dict() if self.obb is not None else None)
