094fc8c24dad14e597d2788ba3229bd4ba5a3109
Wufei Ma <wufeim@gmail.com>
2025-06-29 15:38:26 -0400

    update visualize_infinigen_bbox3d with plot_obb()

--- example_scripts/infinigen/visualize_infinigen_bbox3d.py
+++ example_scripts/infinigen/visualize_infinigen_bbox3d.py
@@ -17,6 +17,31 @@ def parse_args():
     return parser.parse_args()
 
 
+def plot_obb(image, obj, camera_pose_inv, K):
+    corners = obj.obb.corners
+    corners = (
+        camera_pose_inv[:3, :3] @ corners.T
+        + camera_pose_inv[:3, 3:]
+    ).T
+    corners = (K @ corners.T).T
+
+    if corners[:, 2].min() < 0:
+        return
+
+    corners = (corners[:, :2] / corners[:, 2:3]).astype(int)
+
+    for face in FACES:
+        normal = np.cross(
+            corners[face[1]] - corners[face[0]],
+            corners[face[2]] - corners[face[0]])
+        if normal >= 0:
+            continue
+        for i in range(4):
+            start = corners[face[i]]
+            end = corners[face[(i + 1) % 4]]
+            cv2.line(image, tuple(start), tuple(end), (255, 255, 0), 2)
+
+
 def main():
     args = parse_args()
     args.output_path = os.path.join(args.scene_path, args.output_folder)
@@ -32,36 +57,15 @@ def main():
     camera_pose_inv = np.linalg.inv(infinigen_frame.camera_pose)
     K = infinigen_frame.K
     for obj in infinigen_frame.scene.get_all_objects():
-        if infinigen_frame.visibility[obj.uid]:
-            if obj.obb is None:
-                continue
-
-            corners = obj.obb.corners
-            corners = (
-                camera_pose_inv[:3, :3] @ corners.T
-                + camera_pose_inv[:3, 3:]
-            ).T
-            corners = (K @ corners.T).T
-            if corners[:, 2].min() < 0:
-                continue
-            corners = (corners[:, :2] / corners[:, 2:3]).astype(int)
-            corners = corners[:, [0, 1]]
-
-            for face in FACES:
-                normal = np.cross(
-                    corners[face[1]] - corners[face[0]],
-                    corners[face[2]] - corners[face[0]])
-                if normal >= 0:
-                    continue
-                for i in range(4):
-                    start = corners[face[i]]
-                    end = corners[face[(i + 1) % 4]]
-                    plot_image = cv2.line(
-                        plot_image, tuple(start), tuple(end), (255, 255, 0), 2)
+        if not infinigen_frame.visibility[obj.uid]:
+            continue
+        if obj.obb is None:
+            continue
+        plot_obb(plot_image, obj, camera_pose_inv, K)
 
     os.makedirs(args.output_path, exist_ok=True)
     Image.fromarray(np.concatenate([
-        orig_image, plot_image
+        plot_image
     ], axis=1)).save(os.path.join(
         args.output_path,
         f'bbox3d_cam{infinigen_frame.camera_index}_'
