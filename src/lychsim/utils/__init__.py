08a19d5d673baca6a6594ab027f921aef7c953d9
Wufei Ma <wufeim@gmail.com>
2025-06-29 13:20:59 -0400

    add lychsim.utils: colors, general utils, model output

---
+++ src/lychsim/utils/__init__.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from .colors import *
+from .general import rgbd2rgb
+from .model_output import *
+
+
+FACES = np.array([
+    [0, 3, 2, 1],
+    [4, 5, 6, 7],
+    [0, 1, 5, 4],
+    [2, 3, 7, 6],
+    [0, 4, 7, 3],
+    [1, 2, 6, 5],
+])
