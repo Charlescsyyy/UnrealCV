6ffe64b46dab0c9b90bc16d044a0b774b0e91c1c
Wufei Ma <wufeim@gmail.com>
2025-07-01 00:32:09 -0400

    update Object with from_dict and __eq__

--- src/lychsim/core/object.py
+++ src/lychsim/core/object.py
@@ -20,3 +20,15 @@ class Object:
             name=self.name,
             uid=self.uid,
             obb=self.obb.to_dict() if self.obb is not None else None)
+
+    @classmethod
+    def from_dict(cls, data_dict: dict):
+        return cls(
+            name=data_dict['name'], uid=data_dict['uid'],
+            obb=OBB.from_dict(data_dict['obb']) if data_dict['obb'] else None)
+
+    def __eq__(self, other):
+        if not isinstance(other, Object):
+            return False
+        return (self.name == other.name and self.uid == other.uid and
+                self.obb == other.obb)
