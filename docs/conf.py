fd07a0a8c6fa04b90ff967a22d5a148c8255e48c
Wufei Ma <wufeim@gmail.com>
2025-09-05 12:40:47 -0700

    add badge texts

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+from pathlib import Path
+
+from sphinx.application import Sphinx
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -14,8 +18,12 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+sys.path.append(str(Path("_ext").resolve()))
+
 extensions = [
     "sphinx.ext.autodoc",
+    "mybadges",
+    "sphinx_design",
 ]
 
 templates_path = ["_templates"]
@@ -26,6 +34,32 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+html_css_files = ["custom.css", "mycss.css", "mybadges.css"]
 
 html_title = "LychSim"
+
+rst_prolog = """
+.. |area-llm| replace:: :bdg-blue-line:`LLM`
+.. |area-vlm| replace:: :bdg-lightblue-line:`VLM`
+.. |area-rl| replace:: :bdg-purple-line:`Reinforcement Learning`
+.. |area-3d| replace:: :bdg-orange-line:`3D`
+.. |area-pt| replace:: :bdg-green-line:`Pretraining`
+.. |area-dl| replace:: :bdg-amber-line:`Deep Learning`
+.. |area-video| replace:: :bdg-pink-line:`Video`
+.. |area-robot| replace:: :bdg-indigo-line:`Robotics`
+.. |area-gen| replace:: :bdg-deeporange-line:`Generation`
+.. |area-math| replace:: :bdg-teal-line:`Math`
+.. role:: ul
+   :class: ul
+.. role:: bul
+   :class: bul
+.. role:: blue-bul
+   :class: blue-bul
+.. role:: orange
+"""
+
+
+def setup(app: Sphinx) -> None:
+    app.add_css_file("css/custom.css")
+    app.add_css_file("css/mycss.css")
+    app.add_css_file("css/mybadges.css")
